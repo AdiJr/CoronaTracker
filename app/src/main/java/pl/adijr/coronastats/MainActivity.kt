868ab4ed4358cf50307ab344a2b7783c39a724fa
AdiJr <andyguitarist18@gmail.com>
2020-04-18 11:58:48 +0200

    Initial commit

---
+++ app/src/main/java/pl/adijr/coronastats/MainActivity.kt
@@ -0,0 +1,12 @@
+package pl.adijr.coronastats
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.coronastats.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
